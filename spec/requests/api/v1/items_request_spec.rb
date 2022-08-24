11ba2205810d28ddce1229c791c72ce3cc1e32d5
Kat Brandt <joyfulkat11@gmail.com>
2022-08-23 21:06:04 -0600

    Adds Item Show endpoint.

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -44,4 +44,42 @@ describe 'Items API' do
       expect(attributes[:unit_price]).to eq first_item.unit_price
     end
   end
+
+  describe 'GET /items/:id' do
+    it 'returns a single item if id found' do
+      item = create(:item)
+
+      get "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to be_a Hash
+      expect(data).to have_key :data
+
+      item_info = data[:data]
+
+      expect(item_info).to be_a Hash
+      expect(item_info).to have_key :id
+      expect(item_info[:id].to_i).to eq item.id
+
+      expect(item_info).to have_key :type
+      expect(item_info[:type]).to eq "item"
+
+      expect(item_info).to have_key :attributes
+
+      attributes = item_info[:attributes]
+
+      expect(attributes).to be_a Hash
+      expect(attributes).to have_key :name
+      expect(attributes[:name]).to eq item.name
+
+      expect(attributes).to have_key :description
+      expect(attributes[:description]).to eq item.description
+
+      expect(attributes).to have_key :unit_price
+      expect(attributes[:unit_price]).to eq item.unit_price
+    end
+  end
 end
